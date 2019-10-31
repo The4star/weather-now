f16e76365269801d966b841663ec80ae66e69214
root <root@DESKTOP-1ML003O.localdomain>
2019-10-31 13:28:16 +1100

    Updates

---
+++ precache-manifest.01baad17f38c02c528f0784bffacdac5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eba80f87a92226fa6027e0b94465048",
+    "url": "/The4star/weather-now/index.html"
+  },
+  {
+    "revision": "bc37ca7d0b2ce2684b8b",
+    "url": "/The4star/weather-now/static/css/main.21bc9164.chunk.css"
+  },
+  {
+    "revision": "476cc7e6cd4750f36382",
+    "url": "/The4star/weather-now/static/js/2.3e4ba8df.chunk.js"
+  },
+  {
+    "revision": "bc37ca7d0b2ce2684b8b",
+    "url": "/The4star/weather-now/static/js/main.251ed152.chunk.js"
+  },
+  {
+    "revision": "efd2b9dc1cab5930950a",
+    "url": "/The4star/weather-now/static/js/runtime-main.f091267b.js"
+  },
+  {
+    "revision": "baf4f3733f42dab69e1603fa99273208",
+    "url": "/The4star/weather-now/static/media/cloudy-night.baf4f373.mp4"
+  },
+  {
+    "revision": "54e3716821c3e7a723269dda8ee2240a",
+    "url": "/The4star/weather-now/static/media/cloudy.54e37168.mp4"
+  },
+  {
+    "revision": "9ea61d9678c069a3aa4c43f857abb58b",
+    "url": "/The4star/weather-now/static/media/fog.9ea61d96.mp4"
+  },
+  {
+    "revision": "9d10586ea781a0630d6b5c78aff86037",
+    "url": "/The4star/weather-now/static/media/lightning.9d10586e.mp4"
+  },
+  {
+    "revision": "e675606b738277f39b1031a732e08efa",
+    "url": "/The4star/weather-now/static/media/night.e675606b.mp4"
+  },
+  {
+    "revision": "87b503a4615fc55c651ff99c27767fcf",
+    "url": "/The4star/weather-now/static/media/rain.87b503a4.mp4"
+  },
+  {
+    "revision": "06b672ba13d0f332a5d4b49fffd70428",
+    "url": "/The4star/weather-now/static/media/snow.06b672ba.mp4"
+  },
+  {
+    "revision": "a4a62befa61b9135abd5f28138693580",
+    "url": "/The4star/weather-now/static/media/sunny.a4a62bef.mp4"
+  },
+  {
+    "revision": "a2c61991050fc2174fbeaf6763a4c912",
+    "url": "/The4star/weather-now/static/media/tornado.a2c61991.jpg"
+  },
+  {
+    "revision": "8fc136d1020e003c31deb6e0faed476c",
+    "url": "/The4star/weather-now/static/media/windy.8fc136d1.mp4"
+  }
+]);
